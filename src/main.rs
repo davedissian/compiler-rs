8200fcf5caf7ab9b5b85c0c911499ceed7fc9171
David Avedissian <avedissian.david@gmail.com>
2014-10-29 13:20:11 +0000

    Added variable assignments and recollection

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,83 @@
-use std::collections::HashMap;
+mod ast {
+  use std::collections::HashMap;
 
-enum Type {
-  Assign(String, int),
-  Print(String),
-  Block
-}
-
-struct Context {
-  symbol_table: HashMap<String, int>
-}
-
-struct Node {
-  t: Type,
-  children: Vec<Node>
-}
-
-fn set(s: &String, i: int, cxt: &mut Context) {
-  if cxt.symbol_table.contains_key(s) {
-    *cxt.symbol_table.get_mut(s) = i;
-  } else {
-    cxt.symbol_table.insert(s.clone(), i);
+  // Type
+  pub enum Type {
+    Assign(String, int),
+    Print(String),
+    Block
   }
-}
 
-fn lookup(s: &String, cxt: &mut Context) -> int {
-  cxt.symbol_table.get_copy(s)
-}
+  pub struct Node {
+    t: Type,
+    children: Vec<Node>
+  }
 
-fn eval(cxt: &mut Context, n: &Node) {
-  // Match on the type
-  match n.t {
-    Assign(ref s, i) => set(s, i, cxt),
-    Print(ref s) => println!("{} = {}", s, lookup(s, cxt)),
-    Block => for n in n.children.iter() { eval(cxt, n); }
+  // Context
+  pub struct Context {
+    pub symbol_table: HashMap<String, int>
+  }
+
+  impl Context {
+    pub fn new() -> Context {
+      Context {
+        symbol_table: HashMap::new()
+      }
+    }
+
+    fn set(&mut self, s: &String, i: int) {
+      if self.symbol_table.contains_key(s) {
+        *self.symbol_table.get_mut(s) = i;
+      } else {
+        self.symbol_table.insert(s.clone(), i);
+      }
+    }
+
+    fn get(&mut self, s: &String) -> int {
+      self.symbol_table.get_copy(s)
+    }
+  }
+
+  // Constructors
+  pub fn block(cs: Vec<Node>) -> Node {
+    Node {
+      t: Block,
+      children: cs
+    }
+  }
+
+  pub fn assign(s: &str, i: int) -> Node {
+    Node {
+      t: Assign(s.to_string(), i),
+      children: vec!()
+    }
+  }
+
+  pub fn print(s: &str) -> Node {
+    Node {
+      t: Print(s.to_string()),
+      children: vec!()
+    }
+  }
+
+  // Evaluate
+  pub fn eval(cxt: &mut Context, n: &Node) {
+    match n.t {
+      Assign(ref s, i) => cxt.set(s, i),
+      Print(ref s) => println!("{} = {}", s, cxt.get(s)),
+      Block => for n in n.children.iter() { eval(cxt, n); }
+    }
   }
 }
 
 fn main() {
-  let mut context = Context { symbol_table: HashMap::new() };
-  let program = Node {
-    t: Block,
-    children: vec!(
-      Node {t: Assign("x".to_string(), 5), children: vec!()},
-      Node {t: Print("x".to_string()), children: vec!()})
-  };
-  eval(&mut context, &program);
+  let program =
+    ast::block(vec!(
+      ast::assign("x", 5),
+      ast::assign("y", 7),
+      ast::print("x"),
+      ast::print("y"),
+      ast::assign("x", 6),
+      ast::print("x")));
+  ast::eval(&mut ast::Context::new(), &program);
 }
