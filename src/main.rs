b44a50232bf3e504856d46d24de72a7060a2b721
David Avedissian <avedissian.david@gmail.com>
2014-12-29 21:14:03 +0000

    Fixed to work with latest rust version

--- src/main.rs
+++ src/main.rs
@@ -26,36 +26,35 @@ mod ast {
     }
 
     fn set(&mut self, s: &String, i: int) {
-      if self.symbol_table.contains_key(s) {
-        *self.symbol_table.get_mut(s) = i;
-      } else {
-        self.symbol_table.insert(s.clone(), i);
-      }
+      self.symbol_table.insert(s.clone(), i);
     }
 
     fn get(&mut self, s: &String) -> int {
-      self.symbol_table.get_copy(s)
+      match self.symbol_table.get(s) {
+        Some(i) => *i,
+        None => 0
+      }
     }
   }
 
   // Constructors
   pub fn block(cs: Vec<Node>) -> Node {
     Node {
-      t: Block,
+      t: Type::Block,
       children: cs
     }
   }
 
   pub fn assign(s: &str, i: int) -> Node {
     Node {
-      t: Assign(s.to_string(), i),
+      t: Type::Assign(s.to_string(), i),
       children: vec!()
     }
   }
 
   pub fn print(s: &str) -> Node {
     Node {
-      t: Print(s.to_string()),
+      t: Type::Print(s.to_string()),
       children: vec!()
     }
   }
@@ -63,9 +62,9 @@ mod ast {
   // Evaluate
   pub fn eval(cxt: &mut Context, n: &Node) {
     match n.t {
-      Assign(ref s, i) => cxt.set(s, i),
-      Print(ref s) => println!("{} = {}", s, cxt.get(s)),
-      Block => for n in n.children.iter() { eval(cxt, n); }
+      Type::Assign(ref s, i) => cxt.set(s, i),
+      Type::Print(ref s) => println!("{} = {}", s, cxt.get(s)),
+      Type::Block => for n in n.children.iter() { eval(cxt, n); }
     }
   }
 }
